be9d540ebdd0b684a72e37c8c10933bec7b47acd
etczrn <etczrn@gmail.com>
2022-04-05 19:12:21 +0900

    Add infinite scroll

--- src/components/main/post-list.tsx
+++ src/components/main/post-list.tsx
@@ -2,6 +2,9 @@ import React, { FunctionComponent, useMemo } from 'react'
 import styled from '@emotion/styled'
 import PostItem from 'components/main/post-item'
 import { PostListItemType } from 'types/post-item.types'
+import useInfiniteScroll, {
+  useInfiniteScrollType,
+} from 'hooks/use-infinite-scroll'
 
 export type PostType = {
   node: {
@@ -42,23 +45,14 @@ const PostList: FunctionComponent<PostListProps> = function ({
   selectedCategory,
   posts,
 }) {
-  const postListData = useMemo(
-    () =>
-      posts.filter(
-        ({
-          node: {
-            frontmatter: { categories },
-          },
-        }: PostListItemType) =>
-          selectedCategory !== 'All'
-            ? categories.includes(selectedCategory)
-            : true,
-      ),
-    [selectedCategory],
+  const { containerRef, postList }: useInfiniteScrollType = useInfiniteScroll(
+    selectedCategory,
+    posts,
   )
+
   return (
-    <PostListWrapper>
-      {postListData.map(({ node: { id, frontmatter } }: PostListItemType) => (
+    <PostListWrapper ref={containerRef}>
+      {postList.map(({ node: { id, frontmatter } }: PostListItemType) => (
         <PostItem {...frontmatter} link="https://google.co.kr" key={id} />
       ))}
     </PostListWrapper>
