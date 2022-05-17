c8e0c899d4e2a4707b195e7c504b81918a62f099
etczrn <etczrn@gmail.com>
2022-05-17 20:29:21 +0900

    Closes #12; Query Images From Graphql #12

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,10 +1,25 @@
 import * as React from 'react';
-import { Link } from 'gatsby';
+import { Link, graphql } from 'gatsby';
+import { GatsbyImage, getImage } from 'gatsby-plugin-image';
 import Layout from '../components/layout';
 
-export default function AboutPage() {
+export const query = graphql`
+  query CocktailQuery {
+    file(name: { eq: "cocktail" }) {
+      childImageSharp {
+        gatsbyImageData(placeholder: DOMINANT_COLOR)
+      }
+    }
+  }
+`;
+
+export default function AboutPage({ data }) {
   return (
     <Layout>
+      <GatsbyImage
+        image={getImage(data.file)}
+        alt="a cocktail set inside an elaborate floral arrangement with dry ice mist curling around it"
+      />
       <h1>About This Site</h1>
       <Link to="/">Home</Link>
     </Layout>
