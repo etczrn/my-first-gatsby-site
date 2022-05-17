0202e2c145b9af705c408a2cfd36ddad1bef6297
etczrn <etczrn@gmail.com>
2022-05-17 20:09:58 +0900

    Closes #11; Install gatsby remark images and use it in mdx file #11

--- gatsby-config.js
+++ gatsby-config.js
@@ -23,9 +23,19 @@ module.exports = {
         path: `${__dirname}/src/posts`,
       },
     },
+    'gatsby-remark-images',
     {
       resolve: 'gatsby-plugin-mdx',
       options: {
+        // to use gatsby-remark-images in mdx files
+        gatsbyRemarkPlugins: [
+          {
+            resolve: 'gatsby-remark-images',
+            options: {
+              maxWidth: 1200, // limit image width in markdown
+            },
+          },
+        ],
         defaultLayouts: {
           posts: require.resolve('./src/components/post-layout.js'),
         },
